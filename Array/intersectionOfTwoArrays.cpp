0b44461de214922f538d37d917dee2a6969633b1
Aditya Singh <adi11102002@gmail.com>
2022-03-04 23:16:41 +0530

    updated

--- Array/intersectionOfTwoArrays.cpp
+++ Array/intersectionOfTwoArrays.cpp
@@ -1,3 +1,13 @@
+/*
+Question : Given two integer arrays nums1 and nums2, return an array of their intersection. 
+Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.
+
+Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+Output: [4,9]
+Explanation: [9,4] is also accepted.
+*/
+
+
 #include <iostream>
 #include <vector>
 #include <unordered_map>
