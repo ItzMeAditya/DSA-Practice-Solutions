841cffab3728d428ee71d7ca583c3528dfca9471
Aditya Singh <adi11102002@gmail.com>
2022-03-15 23:17:51 +0530

    validParenthesis

--- String/ValidParenthesis.cpp
+++ String/ValidParenthesis.cpp
@@ -1,3 +1,20 @@
+/*
+Questions : Given a string s of '(' , ')' and lowercase English characters.
+Your task is to remove the minimum number of parentheses ( '(' or ')', in any positions ) so that the resulting parentheses string is valid and return any valid string.
+Formally, a parentheses string is valid if and only if:
+
+It is the empty string, contains only lowercase characters, or
+It can be written as AB (A concatenated with B), where A and B are valid strings, or
+It can be written as (A), where A is a valid string.
+
+Example :
+
+Input: s = "lee(t(c)o)de)"
+Output: "lee(t(c)o)de"
+Explanation: "lee(t(co)de)" , "lee(t(c)ode)" would also be accepted.
+*/
+
+
 #include <iostream>
 #include <string>
 using namespace std;
