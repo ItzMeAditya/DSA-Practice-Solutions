42ec461bebbef6ee936bfbfd252e6573acf082a9
Aditya Singh <adi11102002@gmail.com>
2022-03-15 23:23:04 +0530

    updated

--- String/ValidParenthesis.cpp
+++ String/ValidParenthesis.cpp
@@ -7,11 +7,16 @@ It is the empty string, contains only lowercase characters, or
 It can be written as AB (A concatenated with B), where A and B are valid strings, or
 It can be written as (A), where A is a valid string.
 
-Example :
+Example 1:
 
 Input: s = "lee(t(c)o)de)"
 Output: "lee(t(c)o)de"
 Explanation: "lee(t(co)de)" , "lee(t(c)ode)" would also be accepted.
+
+Example 2:
+
+Input: s = "a)b(c)d"
+Output: "ab(c)d"
 */
 
 
