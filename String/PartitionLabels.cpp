9783657b5be0383f1cad2f6ed791ca5010c1be0d
Aditya Singh <adi11102002@gmail.com>
2022-03-21 11:21:21 +0530

    PartitionLabel

---
+++ String/PartitionLabels.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+// function to partition the string into labels 
+vector<int> partitionLabels (string str)
+{
+	// creating unordered map
+	unordered_map <char, int> map;
+	int n = str.length();
+	
+	// storing the last occurence index of characters into map
+	for (int i=0; i<n; i++)
+	{
+		map[str[i]] = i;
+	}
+	
+	vector<int> v;
+	int prev = -1, getMax = 0;
+	for (int i=0; i<n; i++)
+	{
+		char ch = str.at(i);
+		getMax = max(getMax, map.at(ch));
+		
+		if (getMax == i)
+		{
+			v.push_back( getMax - prev );
+			prev = getMax;
+		}
+	}
+	return v;
+}
+
+// driver method
+int main ()
+{
+	int t;
+	cout << "Enter the number of test cases : ";
+	cin >> t;
+	while (t--)
+	{
+		// taking string input from the user
+		string s;
+		cout << "Enter the string : ";
+		cin >> s;
+		
+		// creating vector
+		vector<int> output;
+		output = partitionLabels (s);
+		
+		// printing the output
+		for (int i=0; i<output.size(); i++)
+		{
+			cout << output[i] << " ";
+		}
+		cout << endl;
+	}
+	return 0;
+}
