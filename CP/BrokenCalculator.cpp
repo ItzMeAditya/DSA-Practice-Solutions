f35fadaed28dc9bcb57d63ae0fec0e4dd752192f
Aditya Singh <adi11102002@gmail.com>
2022-03-23 23:37:21 +0530

    updated

--- CP/BrokenCalculator.cpp
+++ CP/BrokenCalculator.cpp
@@ -1,3 +1,19 @@
+/*
+Question : There is a broken calculator that has the integer startValue on its display initially. In one operation, you can:
+
+multiply the number on display by 2, or
+subtract 1 from the number on display.
+Given two integers startValue and target, return the minimum number of operations needed to display target on the calculator.
+
+
+Example :
+
+Input: startValue = 2, target = 3
+Output: 2
+Explanation: Use double operation and then decrement operation {2 -> 4 -> 3}.
+*/
+
+
 #include <iostream>
 using namespace std;
 
